81f6d616e00bc116367550b04962e479d5443904
Parnstermia <Parnstermia@users.noreply.github.com>
2018-10-05 19:34:17 +0200

    Removed legacy code

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,8 +10,4 @@ public class Mover : MonoBehaviour {
         this.GetComponent<Rigidbody>().AddForce(new Vector3(-speed,0,0));
     }
 
-    // Update is called once per frame
-    /*void Update () {
-        this.GetComponent<Transform>().position = new Vector3(this.GetComponent<Transform>().position.x - s, this.GetComponent<Transform>().position.y, this.GetComponent<Transform>().position.z);
-	}*/
 }
